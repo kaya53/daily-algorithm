e36ec4596cc589db271a02a7eeacc604918d4121
kaya <jimin4661@gmail.com>
2023-02-06 21:26:01 +0900

    230206-02

--- 02_SELF/GOLD 4/boj_15961_sushi2/boj_15961.py
+++ 02_SELF/GOLD 4/boj_15961_sushi2/boj_15961.py
@@ -5,29 +5,56 @@ sys.stdin = open('input.txt')
 for _ in range(4):
     # 접시의 수, 초밥의 총 가짓수, 연속해서 먹는 접시 수, 쿠폰 번호
     n, d, k, c = map(int, input().split())
-    sushi = [int(input()) for _ in range(n)]
+    belt = [int(input()) for _ in range(n)]  # 벨트에 놓인 접시 수
+    #
+    # # 초기 작업
+    # cnt_sushi = [0] * (d+1)  # 초밥 번호가 1부터 시작하므로 개수를 하나 더 늘려준다.
+    # cnt_sushi[c] = 1  # 무조건 먹을 수 있는 c번 초밥
+    # for i in range(k):
+    #     cnt_sushi[sushi[i]] += 1
+    # cnt = d - cnt_sushi.count(0) + 1  # cnt_sushi가 d보다 1이 많기 때문에
+    # res = cnt
+    #
+    # # 시작
+    # for i in range(n):  # 모든 접시가 한 번씩 시작점이 되어주어야 하니까
+    #     cnt_sushi[sushi[i]] -= 1  # 빠지는 초밥의 cnt를 빼준다
+    #     if not cnt_sushi[sushi[i]] :
+    #         cnt -= 1  # 지금 초밥의 카운트가 0이라는 건 방금 빠진 초밥말고는 그 초밥이 또 있지 않다는 거니까 카운트도 빼주기
+    #     try:
+    #         if not cnt_sushi[sushi[i+k]]: cnt += 1  # 지금 먹을 초밥이 먹었던 게 아니면 카운트 늘리기
+    #         cnt_sushi[sushi[i+k]] += 1  # 해당 초밥 개수 +1
+    #     except:  # try에서 인덱스 에러가 나면 여기로 오게 될 것
+    #         if not cnt_sushi[sushi[i+k-n]]: cnt += 1
+    #         # 내 코드에서는 에러를 처리하기 위해 계속 % 연산을 했는데
+    #         # 이렇게 하면 에러가 나는 경우에만 -n을 빼서 처리해주면 됨
+    #         cnt_sushi[sushi[i+k-n]] += 1
+    #     if res < cnt: res = cnt
+    #
+    # print(res)
 
     # 초기 작업
-    cnt_sushi = [0] * (d+1)  # 초밥 번호가 1부터 시작하므로 개수를 하나 더 늘려준다.
-    cnt_sushi[c] = 1  # 무조건 먹을 수 있는 c번 초밥
+    sushi = [0] * (d+1)
+    sushi[c] = 1  # 쿠폰이 있으면 무조건 먹기
+    max_cnt = cnt = 1  # 구간 내 초밥의 개수를 센다
     for i in range(k):
-        cnt_sushi[sushi[i]] += 1
-    cnt = d - cnt_sushi.count(0) + 1  # cnt_sushi가 d보다 1이 많기 때문에
-    res = cnt
+        sushi[belt[i]] += 1
+        cnt += 1
+        if sushi[belt[i]] > 1:
+            cnt -= 1
+    # print(sushi) # ok
 
-    # 시작
-    for i in range(n):  # 모든 접시가 한 번씩 시작점이 되어주어야 하니까
-        cnt_sushi[sushi[i]] -= 1  # 빠지는 초밥의 cnt를 빼준다
-        if not cnt_sushi[sushi[i]] :
-            cnt -= 1  # 지금 초밥의 카운트가 0이라는 건 방금 빠진 초밥말고는 그 초밥이 또 있지 않다는 거니까 카운트도 빼주기
-        try:
-            if not cnt_sushi[sushi[i+k]]: cnt += 1  # 지금 먹을 초밥이 먹었던 게 아니면 카운트 늘리기
-            cnt_sushi[sushi[i+k]] += 1  # 해당 초밥 개수 +1
-        except:  # try에서 인덱스 에러가 나면 여기로 오게 될 것
-            if not cnt_sushi[sushi[i+k-n]]: cnt += 1
-            # 내 코드에서는 에러를 처리하기 위해 계속 % 연산을 했는데
-            # 이렇게 하면 에러가 나는 경우에만 -n을 빼서 처리해주면 됨
-            cnt_sushi[sushi[i+k-n]] += 1
-        if res < cnt: res = cnt
+    for i in range(n):
+        ni = (i+k) % n
+        # i번째는 빼고 ni번째는 넣고
+        sushi[belt[i]] -= 1
+        if not sushi[belt[i]]:  # 방금 빠진게 마지막 초밥이었다면 cnt 빼주기
+            cnt -= 1
+        sushi[belt[ni]] += 1
+        if sushi[belt[ni]] == 1:  # 방금 들어온 게 첫 초밥이었다면 cnt 더해주기
+            cnt += 1
 
-    print(res)
+        # 최대값 비교
+        if max_cnt < cnt:
+            max_cnt = cnt
+
+    print(max_cnt)
--- 02_SELF/SILVER 1/boj_2531_sushi/boj_2531.py
+++ 02_SELF/SILVER 1/boj_2531_sushi/boj_2531.py
@@ -21,16 +21,20 @@ for _ in range(4):
 
     # 나올 수 있는 총 구간 n개의 크기를 갖는 배열을 만들고
     # 여기다가 각 구간이 가지는 가지수를 누적한다.
-    cnt_ls = [cnt] + [0]*(n-1)
+    # cnt_ls = [cnt] + [0]*(n-1)
 
     # 0번 인덱스는 초기 작업에서 봤기 때문에 k번 인덱스부터 시작한다.
     # => 접시수와 구간의 수가 같을 때 1부터 시작하면 인덱스 에러가 남; 28번째 줄 n 자리에 8을 써서 그런 것이었음
+    max_cnt = 0
     for i in range(k, n+k-1):
         ri = i % n  # n이 얼마가 나오건 나머지 연산자 처리를 해줘서 한바퀴 돌 수 있게 해준다.
         sect.pop(0)  # 첫번째 원소를 빼고
         sect.append(sushi[ri])  # 그 다음 원소 집어넣기
-        cnt_ls[i-k+1] = count_sushi(sect)  ## 여기서 바로 카운트를 해주면 시간이 줄 수 있다 ; max값 구할 때처럼 바로바로 갱신해 주면 된다
-    print(max(cnt_ls))
+        # cnt_ls[i-k+1] = count_sushi(sect)  ## 여기서 바로 카운트를 해주면 시간이 줄 수 있다 ; max값 구할 때처럼 바로바로 갱신해 주면 된다
+        cnt = count_sushi(sect)
+        if max_cnt < cnt:
+            max_cnt = cnt
+    print(max_cnt)
 
 
 
