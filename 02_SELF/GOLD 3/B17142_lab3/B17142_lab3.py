57f73ce1f5b871c429fa2e3f94cbdc56f9a177bb
kaya53 <jimin4661@gmail.com>
2023-03-05 13:58:58 +0900

    230305

---
+++ 02_SELF/GOLD 3/B17142_lab3/B17142_lab3.py
@@ -0,0 +1,77 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+from collections import deque
+
+def spread(choice, arr, zeros):
+    global mmin
+    now_max = 0
+    # 활성 바이러스 놓기
+    q = deque()
+    for vi, vj in choice:
+        arr[vi][vj] = -3
+        q.append((vi, vj, 0))
+    while q:
+        ci, cj, time = q.popleft()  # 바이러스가 퍼진 점
+        if time >= mmin:  # 최소 시간 이상이면 더 이상 보지 말기
+            return
+        for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            ni, nj = ci + di, cj + dj
+            # 인덱스 밖, 벽, 활성 바이러스를 만난 경우 건너뛰기
+            # 벽, 이미 지나간 곳, 활성 바이러스가 있는 곳은 건너 뛰기
+            if ni < 0 or ni >= n or nj < 0 or nj >= n or (arr[ni][nj] > 0 or arr[ni][nj] == -1 or arr[ni][nj] == -3): continue
+            # if arr[ni][nj] < time + 1: continue
+            q.append((ni, nj, time + 1))
+            if arr[ni][nj] == -2:
+                arr[ni][nj] = -3
+            if not arr[ni][nj]:
+                zeros -= 1
+                arr[ni][nj] = time + 1
+            # 비활성 바이러스이거나 아직 안 지나간 곳인 경우
+    if zeros: return
+    if mmin > time:
+        mmin = time
+    # print(time)
+    # for elem in arr:
+        # print(elem)
+
+
+def activate(idx, si):
+    if idx == m:  # 활성시킬 바이러스를 다 고름
+        spread(choice, [ls[:] for ls in arr], zero_cnt)
+        return
+
+    for i in range(si, len(virus)):
+        choice[idx] = virus[i]
+        activate(idx+1, i+1)
+        choice[idx] = 0
+
+
+for _ in range(12):
+    n, m = map(int, input().split())  # 격자 크기, 놓을 수 있는 활성 바이러스 개수
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    mmin = int(1e9)
+    zero_cnt = n*n
+    # 전체 비활성 바이러스 세기
+    virus = []
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] == 2:  # 모든 바이러스
+                virus.append((i, j))
+                arr[i][j] = -2
+                zero_cnt -= 1
+            # 벽은 -1로 만들어주기
+            if arr[i][j] == 1:
+                arr[i][j] = -1
+                zero_cnt -= 1
+
+    if zero_cnt:
+        choice = [0] * m
+        activate(0, 0)
+        if mmin == int(1e9):
+            print(-1)
+        else:
+            print(mmin)
+    else:
+        print(0)
