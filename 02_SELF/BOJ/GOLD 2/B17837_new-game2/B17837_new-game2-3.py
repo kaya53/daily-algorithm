b71817418e8d8c76c09b3ef7eea2486097e6c0e1
kaya53 <jimin4661@gmail.com>
2023-04-18 08:58:39 +0900

    230418-01

--- 02_SELF/BOJ/GOLD 2/B17837_new-game2/B17837_new-game2-3.py
+++ 02_SELF/BOJ/GOLD 2/B17837_new-game2/B17837_new-game2-3.py
@@ -7,15 +7,11 @@ def move_horse():
     for h in range(1, K+1):
         hi, hj, hd = horses[h]
         nhi, nhj = hi + delta[hd][0], hj + delta[hd][1]
-        now = horses_dict[(hi, hj)]
-        if len(now) >= 4: return False
-        # print(now)
+        now = horses_info[hi][hj]
+
         idx = now.index(h)
         moved = now[idx:]  # 움직일 말들
-        if moved == now:
-            horses_dict.pop((hi, hj))
-        else:
-            horses_dict[(hi, hj)] = now[:idx]
+        horses_info[hi][hj] = now[:idx]
 
         if arr[nhi][nhj] == 0:
             if white_and_red(moved, nhi, nhj) is False:
@@ -24,24 +20,22 @@ def move_horse():
             if white_and_red(moved[::-1], nhi, nhj) is False:
                 return False
         elif arr[nhi][nhj] == 2:
-            blue(moved, hi, hj, hd, h)
+            if blue(moved, hi, hj, hd^1, h) is False:
+                return False
 
 
 def white_and_red(moved, ni, nj):
-    if horses_dict.get((ni, nj), -1) != -1:
-        horses_dict[(ni, nj)] += moved
-    else:
-        horses_dict[(ni, nj)] = moved
+    horses_info[ni][nj] += moved
+
     # 말 위치 정보 갱신
     for no in moved:
         horses[no][0], horses[no][1] = ni, nj
     # 4이상이면 리턴
-    if len(horses_dict[(ni, nj)]) >= 4:
+    if len(horses_info[ni][nj]) >= 4:
         return False
 
 
 def blue(moved, ci, cj, d, no):
-    d = oppo[d]
     ni, nj = ci + delta[d][0], cj + delta[d][1]
     # 방향 바꾸기
     horses[no][-1] = d
@@ -51,25 +45,27 @@ def blue(moved, ci, cj, d, no):
         white_and_red(moved[::-1], ni, nj)
     elif arr[ni][nj] == 2:
         # 다시 원래 대로 horses_dict 돌려 놓기
-        if horses_dict.get((ci, cj), -1) != -1:
-            horses_dict[(ci, cj)] += moved
-        else:
-            horses_dict[(ci, cj)] = moved
+        horses_info[ci][cj] += moved
+        horses[no][-1] = d ^ 1
+        if len(horses_info[ci][cj]) >= 4:
+            return False
 
 
 # input
 delta = [(0, 1), (0, -1), (-1, 0), (1, 0)]
-oppo = [1, 0, 3, 2]
-# for _ in range(5):
+# oppo = [1, 0, 3, 2]
+# for _  in range(5):
 N, K = map(int, input().split())
 blues = [[2] * (N+2)]
 arr = blues + [[2] + list(map(int, input().split())) + [2] for _ in range(N)] + blues
 horses = [0] * (K+1)
-horses_dict = {}
+horses_info = [[[] for _ in range(N+2)] for _ in range(N+2)]
+# horses_dict = {}
 for kk in range(1, K+1):
     r, c, dd = map(int, input().split())
     horses[kk] = [r, c, dd-1]
-    horses_dict[(r, c)] = [kk]
+    # horses_dict[(r, c)] = [kk]
+    horses_info[r][c].append(kk)
 
 # main
 for turn in range(1, 1001):
