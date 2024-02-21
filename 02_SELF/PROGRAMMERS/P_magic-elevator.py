2b35ce7df3bbd7c116c715c2baa0fbac5cf81659
kaya53 <jimin4661@gmail.com>
2024-02-21 15:32:27 +0900

    240221

---
+++ 02_SELF/PROGRAMMERS/P_magic-elevator.py
@@ -0,0 +1,22 @@
+answer = int(1e9)
+
+
+def recur(num, tot):
+    global answer
+    # print(num, tot)
+    if num <= 5:
+        answer = min(answer, tot + num)
+        return
+    num, v = divmod(num, 10)
+    if v == 5:
+        recur(num + 1, tot + 5)
+        recur(num, tot + 5)
+    elif v > 5:
+        recur(num + 1, tot + (10 - v))
+    else:
+        recur(num, tot + v)
+
+
+def solution(storey):
+    recur(storey, 0)
+    return answer
